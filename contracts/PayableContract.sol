9b85d7ff6fc078e9dfc402d8abbffcd2b1d31de4
Agustín <acostaagustin6499@gmail.com>
2025-11-25 00:39:41 -0300

    Enhance PayableContract with events and error handling
    Added error handling and events for deposits and withdrawals.

--- contracts/PayableContract.sol
+++ contracts/PayableContract.sol
@@ -1,13 +1,33 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.24; 
 
+error TransferFailed();
+error InsufficientBalance(uint256 requested, uint256 available);
+
 contract PayableContract {
 
-    function sendEther() public payable {}
+    event Withdrawal(address indexed to, uint256 amount);
+    event Deposit(address indexed from, uint256 amount);
 
-    function withdrawEther(uint256 amount) public {
-        (bool success, ) = msg.sender.call{value: amount}("");
-        require (success, "Transfer failed");
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value);
     }
 
+    function sendEther() public payable {
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdrawEther(uint256 amount) public {
+        if (address(this).balance < amount) {
+            revert InsufficientBalance(amount, address(this).balance);
+        }
+
+        (bool success, ) = msg.sender.call{value: amount}("");
+        
+        if (!success) {
+            revert TransferFailed();
+        }
+
+        emit Withdrawal(msg.sender, amount);
+    }
 }
